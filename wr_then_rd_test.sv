4da7279aa2f852e91cac2f49df1de27446d817cb
Vũ Tuấn Hùng <125846103+vutuanhungkkk@users.noreply.github.com>
2025-02-22 15:12:32 +0700

    Update wr_then_rd_test.sv

--- wr_then_rd_test.sv
+++ wr_then_rd_test.sv
@@ -1,21 +1,26 @@
-class fifo_wr_rd_parll_test extends fifo_test;
-  `uvm_component_utils(fifo_wr_rd_parll_test)
-  
-	fifo_wr_rd_parallel_seq seq;
+class fifo_wr_then_rd_test extends fifo_test;
 
-  function new(string name = "fifo_wr_rd_parll_test",uvm_component parent=null);
+  `uvm_component_utils(fifo_wr_then_rd_test)
+  
+  fifo_wr_then_rd_sequence seq;
+
+  function new(string name = "fifo_wr_rd_test",uvm_component parent=null);
     super.new(name,parent);
   endfunction : new
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-        seq = fifo_wr_rd_parallel_seq::type_id::create("seq");
+
+    seq = fifo_wr_then_rd_sequence::type_id::create("seq");
   endfunction : build_phase
+  
   task run_phase(uvm_phase phase);
+    
     phase.raise_objection(this);
     seq.start(env.agt.seqr);
     phase.drop_objection(this);
+
     phase.phase_done.set_drain_time(this, 50);
   endtask : run_phase
   
-endclass : fifo_wr_rd_parll_test
+endclass : fifo_wr_then_rd_test
